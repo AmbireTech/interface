2c882fe940c61fa639c69b48a6cf3f22e1b1c062
IvoPaunov <IvoPaunov@users.noreply.github.com>
2022-10-07 14:19:10 +0000

    deploy: 8a5b666412ae65b8cf63ad17996aaca5ea1dbb94

---
+++ staging/static/js/43.1a83d709.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[43],{1580:function(s,e){s.exports={messages:{}}}}]);
+//# sourceMappingURL=43.1a83d709.chunk.js.map
