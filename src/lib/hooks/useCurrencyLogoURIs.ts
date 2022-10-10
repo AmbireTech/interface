81ed8c50bb64a2176ffc312f173aa7f9596db1c2
Borislav Itskov <borislav@devlabs.bg>
2022-10-10 16:43:04 +0300

    fix: run eslint

--- src/lib/hooks/useCurrencyLogoURIs.ts
+++ src/lib/hooks/useCurrencyLogoURIs.ts
@@ -8,13 +8,22 @@ import EthereumLogo from '../../assets/images/ethereum-logo.png'
 import AvaxLogo from '../../assets/svg/avalanche-avax-logo.svg'
 import BinanceLogo from '../../assets/svg/bnb-logo.svg'
 import CeloLogo from '../../assets/svg/celo_logo.svg'
+import FantomLogo from '../../assets/svg/fantom-logo.svg'
 import MaticLogo from '../../assets/svg/matic-token-icon.svg'
 import MoonbeamLogo from '../../assets/svg/moonbeam-token.svg'
 import MoonriverLogo from '../../assets/svg/moonriver-logo.svg'
-import FantomLogo from '../../assets/svg/fantom-logo.svg'
 import { isCelo, nativeOnChain } from '../../constants/tokens'
 
-type Network = 'ethereum' | 'arbitrum' | 'optimism' | 'polygon' | 'avalanche' | 'binance' | 'moonbeam' | 'moonriver' | 'fantom'
+type Network =
+  | 'ethereum'
+  | 'arbitrum'
+  | 'optimism'
+  | 'polygon'
+  | 'avalanche'
+  | 'binance'
+  | 'moonbeam'
+  | 'moonriver'
+  | 'fantom'
 
 function chainIdToNetworkName(networkId: SupportedChainId): Network {
   switch (networkId) {
