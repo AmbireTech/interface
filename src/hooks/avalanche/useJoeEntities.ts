c1f2c39f0a198797f5e346d95377db65af059a27
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2022-10-17 18:35:55 +0300

    feat: add weth, usdc bridged and tether to hop assets

--- src/hooks/avalanche/useJoeEntities.ts
+++ src/hooks/avalanche/useJoeEntities.ts
@@ -7,10 +7,14 @@ import { WRAPPED_NATIVE_CURRENCY } from 'constants/tokens'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 
 const swapHopAssets = [
-  { address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', name: 'wavax', symbol: 'WAVAX', decimals: 18 },
-  { address: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', name: 'usdc', symbol: 'USCD', decimals: 6 },
+  { address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', name: 'Wrapped AVAX', symbol: 'WAVAX', decimals: 18 },
+  { address: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E', name: 'USD Coin', symbol: 'USCD', decimals: 6 },
+  { address: '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664', name: 'USD Coin - Bridged', symbol: 'USDC.e', decimals: 6 },
   { address: '0xd586E7F844cEa2F87f50152665BCbc2C279D8d70', name: 'dai', symbol: 'DAI', decimals: 18 },
   { address: '0x63a72806098Bd3D9520cC43356dD78afe5D386D9', name: 'aave', symbol: 'AAVE', decimals: 18 },
+  { address: '0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB', name: 'weth', symbol: 'WETH.e', decimals: 18 },
+  { address: '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7', name: 'Tether', symbol: 'USDT', decimals: 6 },
+  { address: '0xc7198437980c041c805A1EDcbA50c1Ce5db95118', name: 'Tether - Bridged', symbol: 'USDT.e', decimals: 6 },
 ]
 
 export function useGetCurrency(v2currency: V2Token | V2NativeCurrency | undefined): Token | Currency | undefined {
