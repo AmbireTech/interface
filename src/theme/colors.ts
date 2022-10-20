2a1d6d5598003d983eb46aad5956b9b52157dd94
IvoPaunov <ivo.paunov@gmail.com>
2022-10-20 16:56:24 +0300

    update colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -288,8 +288,8 @@ export const colorsLight: Palette = {
     '8px 12px 20px rgba(51, 53, 72, 0.04), 4px 6px 12px rgba(51, 53, 72, 0.02), 4px 4px 8px rgba(51, 53, 72, 0.04);',
   shallowShadow:
     '6px 6px 10px rgba(51, 53, 72, 0.01), 2px 2px 6px rgba(51, 53, 72, 0.02), 1px 2px 2px rgba(51, 53, 72, 0.02);',
-  stateOverlayHover: opacify(8, colors.gray300),
-  stateOverlayPressed: opacify(24, colors.gray200),
+  stateOverlayHover: opacify(70, '#191a2b'),
+  stateOverlayPressed: opacify(90, '#191a2b'),
 }
 
 export const colorsDark: Palette = {
@@ -349,6 +349,6 @@ export const colorsDark: Palette = {
 
   deepShadow: '12px 16px 24px rgba(0, 0, 0, 0.24), 12px 8px 12px rgba(0, 0, 0, 0.24), 4px 4px 8px rgba(0, 0, 0, 0.32);',
   shallowShadow: '4px 4px 10px rgba(0, 0, 0, 0.24), 2px 2px 4px rgba(0, 0, 0, 0.12), 1px 2px 2px rgba(0, 0, 0, 0.12);',
-  stateOverlayHover: opacify(8, colors.gray300),
-  stateOverlayPressed: opacify(24, colors.gray200),
+  stateOverlayHover: opacify(70, '#191a2b'),
+  stateOverlayPressed: opacify(90, '#191a2b'),
 }
