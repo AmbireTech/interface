49356f81527e053d8e0c63106540e66d3d5ba3ec
borislav-itskov <borislav-itskov@users.noreply.github.com>
2022-10-31 08:09:37 +0000

    deploy: 411c30ae82d5344fad55f7aeb428ffeb6466a18d

--- staging/static/js/43.d859ea17.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[43],{1621:function(s,e){s.exports={messages:{}}}}]);
-//# sourceMappingURL=43.d859ea17.chunk.js.map
--- staging/static/js/6.6dba8eac.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[6],{1795:function(n,e,r){"use strict";r.d(e,"b",(function(){return t})),r.d(e,"a",(function(){return i}));r(338),r(1731);var t="_2kdvfw1",i="_2kdvfw3 _2kdvfw1 rgw6ez69u rgw6ez68m rgw6ez4am"}}]);
-//# sourceMappingURL=6.6dba8eac.chunk.js.map
